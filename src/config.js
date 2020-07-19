9a23834a3a4444befd944c646e40a227a674f221
Vazgen Karapetyan <vkarapetyan@irobot.com>
2020-07-19 01:24:30 -0700

    Creating folder for storing keys.

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
     GQL_HOSTNAME: 'localhost',
     GQL_PORT: 4000,
@@ -7,7 +9,7 @@ module.exports = {
     USERSPACE_DIR_ROOT : '/home/metriffic/userspace/',
     USERSPACE_HOST : '192.168.86.199',
 
-    WORKSPACE_MANAGER_PRIVATE_KEY_FILE: 'workspace_manager_private.key',
+    WORKSPACE_MANAGER_PRIVATE_KEY_FILE: path.join('keys', 'workspace_manager_private.key'),
     WORKSPACE_MANAGER_ENDPOINT: 'workspace_manager',
 
     RSYNC_SERVER_PORT : 2222
