23fae82875e94bb3de7c4f7eb5742b4c8e6af585
VK <metriffic@metriffic.com>
2024-11-07 10:54:00 -0800

    Using the same env variable for USERSPACE as other services

--- src/config.js
+++ src/config.js
@@ -5,11 +5,11 @@ module.exports = {
     GQL_ADDRESS: env.get('METRIFFIC_GQL_ADDRESS').required().asString(),
     GQL_ENDPOINT: 'workspace_manager',
 
-    USERSPACE_ROOT: env.get('METRIFFIC_USERSPACE_ROOT').required().asString(),
+    USERSPACE_ROOT: env.get('METRIFFIC_USERSPACE_NFS_ROOT').required().asString(),
 
     WORKSPACE_MANAGER_PRIVATE_KEY_FILE: env.get('METRIFFIC_WORKSPACE_MANAGER_PRIVATE_KEY_FILE').required().asString(),
 
     RSYNC_SERVER_HOST_KEY_FILE: env.get('METRIFFIC_RSYNC_SERVER_HOST_KEY_FILE').required().asString(),
     RSYNC_SERVER_PORT : 7000
 }
-  
+
